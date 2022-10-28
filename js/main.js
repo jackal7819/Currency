eab47ae9b64f9c0039d5e34611ad8f4e810b4ac3
Viktor <106143167+jackal7819@users.noreply.github.com>
2022-10-28 18:23:50 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,46 +1,29 @@
-'use strict';
-
-let url = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json';
 let newValue = document.createElement('h4');
 let output = 0;
-let rateCurrency = 0;
-let input = document.querySelector('#input');
-let inputValue = Number(input.value);
-let btnCurrency = document.querySelectorAll('.exchange__button');
-fetch(url)
-    .then((response) => response.json())
-    .then((currency) => {
-        currency.forEach((item) => {
-            if (
-                item.cc === 'TRY' ||
-                item.cc === 'EGP' ||
-                item.cc === 'EUR' ||
-                item.cc === 'USD' ||
-                item.cc === 'PLN'
-            ) {
-                amountCurrency(item);
-            }
-        });
-    });
 
-function amountCurrency(item) {
+function amountCurrency() {
+
+    let input = document.querySelector('#input');
+    let inputValue = Number(input.value);
+    let btnCurrency = document.querySelectorAll('.exchange__button');
     for (let elem of btnCurrency) {
         elem.addEventListener('click', () => {
-            if (elem.getAttribute('data-rate') === item.cc) {
-                exchangeRate(elem);
-            }
-        });
-
+            exchangeRate(elem);
+        })
         
-        function exchangeRate(elem) {
-            rateCurrency = item.rate;
+        let rateCurrency = elem.getAttribute('data-rate');
+        let exchange = document.querySelector('.exchange');
+    
+        exchange.append(newValue);
+        newValue.innerHTML = `You can buy: ${output}`;
+    
+        function exchangeRate(elem){
+    
             output = Math.floor(inputValue / rateCurrency);
             newValue.replaceWith(newValue);
             newValue.innerHTML = `You can buy: ${output} ${elem.textContent}`;
         }
-        
-        let exchange = document.querySelector('.exchange');
-        exchange.append(newValue);
-        newValue.innerHTML = `You can buy: ${output}`;
     }
 }
+
+amountCurrency();
