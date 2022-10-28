3dd1989e551d8f1a024fde8d30200028fa48d7f0
Viktor <106143167+jackal7819@users.noreply.github.com>
2022-10-28 11:57:33 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,45 @@
+'use strict';
+
+let url = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json';
+let newValue = document.createElement('h4');
+let output = 0;
+let rateCurrency = 0;
+let input = document.querySelector('#input');
+let inputValue = Number(input.value);
+let btnCurrency = document.querySelectorAll('.exchange__button');
+fetch(url)
+    .then((response) => response.json())
+    .then((currency) => {
+        currency.forEach((item) => {
+            if (
+                item.cc === 'TRY' ||
+                item.cc === 'EGP' ||
+                item.cc === 'EUR' ||
+                item.cc === 'USD' ||
+                item.cc === 'PLN'
+            ) {
+                amountCurrency(item);
+            }
+        });
+    });
+
+function amountCurrency(item) {
+    for (let elem of btnCurrency) {
+        elem.addEventListener('click', () => {
+            if (elem.getAttribute('data-rate') === item.cc) {
+                rateCurrency = item.rate;
+                exchangeRate(elem);
+            }
+        });
+
+        let exchange = document.querySelector('.exchange');
+        exchange.append(newValue);
+        newValue.innerHTML = `You can buy: ${output}`;
+    }
+}
+
+function exchangeRate(elem) {
+    output = Math.floor(inputValue / rateCurrency);
+    newValue.replaceWith(newValue);
+    newValue.innerHTML = `You can buy: ${output} ${elem.textContent}`;
+}
